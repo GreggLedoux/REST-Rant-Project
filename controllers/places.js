5e524a2d9cf46bb57a69bca08938e0d6aa3d4c89
GreggLedoux <guitlouie@gmail.com>
2023-02-23 19:42:01 -0500

    Jazzed up form page

--- controllers/places.js
+++ controllers/places.js
@@ -1,8 +1,26 @@
 const router = require('express').Router()
+const places = require('../models/places')
 
+router.get('/new', (req, res) => {
+    res.render('places/new')
+  })
+  
 app.get('/', (req, res) => {
-    let places = []
-    res.render('places/index')
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: '/images/tables.jpg'
+      }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: '/images/coffee.jpg'
+      }]
+      
+    res.render('place/index', { places })
 })
 
 module.exports = router
